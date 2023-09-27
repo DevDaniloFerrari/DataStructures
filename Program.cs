1fe189f224c415624a8d992ce68df15c58e2f4b7
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2023-09-27 19:46:08 -0300

    implement the concept of pages

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace DataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListExample();
+        }
+
+        public static void LinkedListExample()
+        {
+            var pages = new LinkedList<string>();
+
+            pages.AddLast("youtube");
+            pages.AddLast("facebook");
+            pages.AddLast("instagram");
+            pages.AddLast("email");
+            pages.AddLast("azure");
+
+            var last = pages.Last();
+            var atualPage = pages.FindLast(last);
+
+            Console.WriteLine(atualPage.Value);
+            Console.WriteLine(atualPage.Previous.Value);
+        }
+
+    }
+}

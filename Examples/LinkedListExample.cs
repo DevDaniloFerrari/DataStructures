901d2863b051ab4fdbec2cdd4c5fbaf647283a8c
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2023-09-28 19:53:53 -0300

    implementing an linked list class example

---
+++ Examples/LinkedListExample.cs
@@ -0,0 +1,55 @@
+﻿namespace DataStructures.Examples
+{
+    public class LinkedListExample
+    {
+        private LinkedList<string> Pages { get; set; }
+        private LinkedListNode<string> AtualPage { get; set; }
+
+        public LinkedListExample()
+        {
+            Pages = new LinkedList<string>();
+        }
+        //public void Run()
+        //{
+
+        //    pages.AddLast("youtube");
+        //    pages.AddLast("facebook");
+        //    pages.AddLast("instagram");
+        //    pages.AddLast("email");
+        //    pages.AddLast("azure");
+
+        //    NavigateTo("google");
+        //}
+
+        public void NavigateTo(string page)
+        {
+            Pages.AddLast(page);
+            ChangeAtualPage();
+        }
+
+        public string Forward()
+        {
+            if (AtualPage == null)
+                return "There is no page to moving forward!";
+
+            AtualPage = AtualPage.Next;
+            return AtualPage?.Value;
+        }
+
+        public string Backward()
+        {
+            if (AtualPage == null)
+                return "There is no page to moving backward!";
+
+            AtualPage = AtualPage.Previous;
+            return AtualPage?.Value;
+
+        }
+
+        private void ChangeAtualPage()
+        {
+            var last = Pages.Last();
+            AtualPage = Pages.FindLast(last);
+        }
+    }
+}

0083f826847b77c52fe47b48d4693c696f5ee45a
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2023-10-06 18:55:41 -0300

    implementing stack methods

--- Examples/StackExample.cs
+++ Examples/StackExample.cs
@@ -3,25 +3,54 @@
     public class StackExample
     {
         public string[] Stack { get; set; }
+        public int Top { get; set; }
+        public int MaxSize { get; set; }
 
         public StackExample(int size)
         {
             Stack = new string[size];
+            Top = -1;
+            MaxSize = size;
         }
 
         public void Push(string value)
         {
+            if (IsFull())
+            {
+                Console.WriteLine("Stack Overflow");
+                return;
+            }
 
+            Stack[Top++] = value;
         }
 
-        public void Pop()
+        public string Pop()
         {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack Underflow");
+                return null;
+            }
 
+            var value = Stack[Top];
+            Stack[Top] = null;
+            Top = Top--;
+
+            return value;
         }
 
-        public void Peek()
+        public string Peek()
         {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack Underflow");
+                return null;
+            }
 
+            return Stack[Top];
         }
+
+        private bool IsFull() => Top + 1 == MaxSize;
+        private bool IsEmpty() => Top == -1;
     }
 }
